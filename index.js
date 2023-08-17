77474ba6ec4da34c54805df10ef8ec2ecd35d7ac
Kalifunk <nathansing.ns@gmail.com>
2023-08-17 16:47:28 +1000

    re-did CORS policy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const cors = require("cors");
 const path = require("path");
+const cors = require("cors");
 
 const createCheckoutSession = require("./api/checkout");
 const webhook = require("./api/webhook");
@@ -13,14 +13,16 @@ const updatePaymentIntent = require("./api/updatePaymentIntent");
 
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
-// const corsOptions = {
-//   origin: "*",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-
 const app = express();
 const port = 8080;
+
+const corsOptions = {
+  origin: true,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(
   express.json({
@@ -28,36 +30,7 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://superlative-crostata-89b75d.netlify.app"
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 app.use(decodeJWT);
-
-// app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.post("/save-payment-method", validateUser, setupIntent);
 app.post("/webhook", webhook);
