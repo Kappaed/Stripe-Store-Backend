549104b88b3e7ac275dad4fa100ff2c78ec23e09
Kalifunk <nathansing.ns@gmail.com>
2023-08-17 16:50:10 +1000

    re-did CORS policy

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const app = express();
 const port = 8080;
 
-const corsOptions = {
-  origin: true,
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: true,
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(
   express.json({
