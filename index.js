f9e9498e10fd54bbbc40eb4159b030e1639944e8
Kalifunk <nathansing.ns@gmail.com>
2023-08-17 16:28:14 +1000

    added CORS rules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const cors = require("cors");
+
 const createCheckoutSession = require("./api/checkout");
 const webhook = require("./api/webhook");
 const paymentIntent = require("./api/paymentIntent");
@@ -9,7 +11,15 @@ const setupIntent = require("./api/setupIntent");
 const validateUser = require("./auth/validateUser");
 const getCards = require("./api/getPaymentMethod");
 const updatePaymentIntent = require("./api/updatePaymentIntent");
+
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 const port = 8080;
 
@@ -21,7 +31,10 @@ app.use(
 
 app.use(decodeJWT);
 
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 app.use(cors({ origin: true }));
+
 app.post("/save-payment-method", validateUser, setupIntent);
 app.post("/webhook", webhook);
 app.post("/create-payment-intent", paymentIntent);
