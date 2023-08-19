e822640aa7b5d2dd67d8652abe47ae68534076d4
Kalifunk <nathansing.ns@gmail.com>
2023-08-19 21:21:17 +1000

    try to fix CORS error

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const app = express();
 const port = 8080;
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.options("*", cors(corsOptions));
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.options("*", cors());
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(
   express.json({
